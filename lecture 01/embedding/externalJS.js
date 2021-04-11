9a47f6fb01c3717a2911d8b77139d83f25c9cdd8
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2021-04-11 06:54:40 +0200

    lecture 01 example updated

--- lecture 01/embedding/externalJS.js
+++ lecture 01/embedding/externalJS.js
@@ -5,3 +5,8 @@ alert("Hello World")
 alert("Second Command")
 
 alert("Third Command")
+
+console.log(document)
+console.log(document.body)
+console.log(document.doctype)
+console.log(document.title)
