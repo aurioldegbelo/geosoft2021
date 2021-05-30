c433c9ca385355000901622472ac8f8d743a5627
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2021-05-30 22:03:06 +0200

    examples for lecture 08 updated

---
+++ lecture 08/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+//const path = require('path')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => 
+{
+  res.send('Hello World!')
+})
+
+app.use(express.static('public'))
+//app.use('/static', express.static('public'))
+//app.use('/static', express.static(path.join(__dirname, 'public')))
+
+
+app.listen(port, () => 
+{
+  console.log(`App listening at http://localhost:${port}`)
+})
+
