b99c36948d88b34a1dcfda2462b86428303399f6
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2021-05-31 18:59:52 +0200

    folder structure for lecture 08 updated

--- lecture 08/app.js
+++ lecture 08/app.js
@@ -1,17 +1,12 @@
 const express = require('express')
-//const path = require('path')
+const path = require('path')
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => 
-{
-  res.send('Hello World!')
-})
 
-app.use(express.static('public'))
-//app.use('/static', express.static('public'))
-//app.use('/static', express.static(path.join(__dirname, 'public')))
+// rendering static files, e.g. brandenburg_gate
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
 app.listen(port, () => 
