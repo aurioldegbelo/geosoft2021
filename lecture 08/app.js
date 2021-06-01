d0d001ebff0c4ce167725794e5573aeb6986d924
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2021-06-01 20:57:11 +0200

    app.js instructions updated

--- lecture 08/app.js
+++ lecture 08/app.js
@@ -12,5 +12,8 @@ app.use('/static', express.static(path.join(__dirname, 'public')))
 app.listen(port, () => 
 {
   console.log(`App listening at http://localhost:${port}`)
+  console.log(`Since we have only one route, named /static/, use /static/filename to show a file from the public folder`)
+  console.log(`E.g. /static/index.html will render index.html file`)
+  console.log(`You can also change /static/ to / into the route definition (line 9). Things will be more convenient then :)`)
 })
 
