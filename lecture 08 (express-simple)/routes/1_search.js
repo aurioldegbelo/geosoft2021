ee175e1c09d5ac22fba0627fcfe938f4c25d85ef
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2021-06-01 20:57:32 +0200

    search file updated

--- lecture 08 (express-simple)/routes/1_search.js
+++ lecture 08 (express-simple)/routes/1_search.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET search page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) 
+{
   res.render('1_search', { title: 'Search' });
 });
 
